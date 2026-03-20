93428bde0719c36350c2461aace092b744f7c012
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-20 13:02:38 +0200

    Solution

---
+++ musician/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+    is_adult = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Musician
+        fields =[
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult"
+        ]
+
+    def get_is_adult(self, obj: Musician) -> bool:
+        return obj.is_adult
